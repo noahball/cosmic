68b0b5fbc6a7c305bcc2b97ac65a02cb43d6d73d
Noah Ball <noahb.home@gmail.com>
2020-10-19 19:48:33 +1300

    Initial

---
+++ src/components/AboutMe.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function AboutMe() {
+    return (
+        <div class="about">
+            <h1>All About Noah</h1>
+            <p>Heya, I'm an avid computer programmer from New Zealand. I enjoy programming in a variety of languages, especially JavaScript. You can see me [in very rare occasions] outside at the beach, or walking around town!</p>
+            <h3>My top dream holiday spots</h3>
+            <ol>
+                <li>Silicon Valley</li>
+                <li>Rome</li>
+                <li>Paris</li>
+            </ol>
+        </div>
+    )
+}
+
+export default AboutMe
